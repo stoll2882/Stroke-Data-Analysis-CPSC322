6ba232d18214e9085a0d2088058ac8f4c2e7638b
Sabrina Ollestad <sollestad@zagmail.gonzaga.edu>
2021-05-01 05:13:04 +0000

    bar chart?

--- plot_utils.py
+++ plot_utils.py
@@ -12,3 +12,19 @@ def pie_chart(x, y, title):
     plt.pie(y, labels=x, autopct="%1.1f%%")
     plt.title(title)
     plt.show()
+
+
+def bar_chart(x, y, title):
+    '''
+    This function creates a standard bar chart
+
+    Attributes:
+        x: list of x axis labels
+        y: list of corresponding y values 
+        title: title for the chart
+    '''
+    plt.figure()
+    plt.bar(x, y)
+    plt.xticks(rotation=45, horizontalalignment="right")
+    plt.title(title)
+    plt.show()
